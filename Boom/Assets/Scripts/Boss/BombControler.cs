0a6e5b45915327e9ac4a5ed64562f7bba3b62b30
tranquocdinhcse98 <33130805+tranquocdinhcse98@users.noreply.github.com>
2017-11-20 17:20:40 +0700

    Boss bắn bom
    Tường bắn xoay và bắn bom theo nhân vật

---
+++ Boom/Assets/Scripts/Boss/BombControler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombControler : MonoBehaviour {
+
+    public Vector3 target;
+    public float moveSpeed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate((transform.position - target) * moveSpeed * Time.deltaTime* - 1) ;//Xác định vị trí của nhân vật để ném bom
+	}
+}
---
+++ Boom/Assets/Scripts/Boss/BossHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour {
+
+    public float maxHealth;
+    float currHealth;
+    public Slider BossHealthSlider;//Thanh máu UI
+	// Use this for initialization
+	void Start () {
+        currHealth = maxHealth;
+        BossHealthSlider.maxValue = maxHealth;
+        BossHealthSlider.value = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddDamge(float damage)
+    {
+        currHealth -= damage; //Máu giảm theo lượng damage do bom gây ra
+        BossHealthSlider.value = currHealth; //Thanh máu giảm
+        if (currHealth <= 0) MakeDead();//Chết
+    }
+
+    void MakeDead()
+    {
+        Destroy(gameObject);
+    }
+}
